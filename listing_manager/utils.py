7a707aa38cf36efe1d6bebc94c74f9f475686468
mikezareno <33703499+mikezareno@users.noreply.github.com>
2020-07-03 17:09:22 -0400

    Update utils.py

--- listing_manager/utils.py
+++ listing_manager/utils.py
@@ -10,15 +10,15 @@ from six import string_types
 class InvalidWarehouseCompany(frappe.ValidationError): pass
 
 @frappe.whitelist()
-def get_item_code(scancode=None)
-	global item_code
-	#try barcode lookup
-	item_code = frappe.db.get_value("Item Barcode", {"scancode": barcode}, fieldname=["parent"])
-	if not item_code:
-	#try supplier code lookup
-	item_code = frappe.db.get_value("Item Supplier", {"scancode": supplier_part_no}, fieldname=["parent"]) 
-	if not item_code:
-		frappe.throw(_("No Item Found").format(scancode))
+def get_item_code(scancode=None):
+	if scancode:
+		#try barcode lookup
+		item_code = frappe.db.get_value("Item Barcode", {"scancode": barcode}, fieldname=["parent"])
+		if not item_code:
+			#try supplier code lookup
+			item_code = frappe.db.get_value("Item Supplier", {"scancode": supplier_part_no}, fieldname=["parent"]) 
+		if not item_code:
+			frappe.throw(_("No Item Found").format(scancode))
 
 	return item_code
 
